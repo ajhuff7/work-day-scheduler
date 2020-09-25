c4effbc4635585ffbce78b1b8fa371fb1fbe2bdb
ajhuff7 <70546019+ajhuff7@users.noreply.github.com>
2020-09-24 21:29:54 -0700

    set scheduler date and running clock

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
 $(document).ready(function () {
 
+    // set date and time on scheduler
     $("#currentDay").text(moment().format('MMMM Do YYYY, h:mm:ss a'));
 
+    // make function to run hh:mm:ss on clock
     let updateTime = function () {
         let currentTime = moment().format('MMMM Do YYYY, h:mm:ss a')
         $("#currentDay").text(currentTime)
     }
-
+    // call the function to run...
     updateTime();
 
-    // To continuously call the functions, we will use setInterval
+    // This will keep the clock running...
     setInterval(updateTime, 1000);
+
+
+
+    
 });
