ad9a7036bac0be35260106131a968db66084613f
ajhuff7 <70546019+ajhuff7@users.noreply.github.com>
2020-09-26 21:06:56 -0700

    functioning current colors

--- README.md
+++ README.md
@@ -1,1 +1,70 @@
-# work-day-scheduler
+# Work Day Scheduler
+
+A daily scheduler with a date field and clock that shows hours, minutes, and seconds. Designated areas for input of of daily tasks arranged by hour which highlight current activities separately from past and future events.
+
+## Table of Contents
+
+* [Tech Used](#tech_used)
+* [Usage](#usage)
+* [Features](#features)
+* [Deployed_Link](#deployed_link)
+* [Author](#author)
+* [Credits](#credits)
+* [License](#license)
+
+----
+
+## Tech Used
+
+* [HTML](https://developer.mozilla.org/en-US/docs/Web/HTML)
+* [Bootstrap](https://getbootstrap.com/)
+* [Javascript](https://developer.mozilla.org/en-US/docs/Web/JavaScript)
+* [JQuery](https://jquery.com/)
+* [Moment.js](https://momentjs.com/)
+* [Font_Awesome](https://fontawesome.com/)
+
+## Usage
+ 
+ This scheduler will allow text input into hourly columns and show the user current and upcoming events as well as past entries. The data utilizes local storage to recall data and continue to display even on page refresh. A running calendar and clock display current date and time.
+
+## Features
+
+- Dynamic code to display current date and time.
+- Area for text entry of calendar events designated my hour.
+
+- Save button to save entries to local storage.
+
+  ![code_Snippet.1](https://github.com/ajhuff7/portfolio-one/blob/master/assets/images/code_Snippet.1.png)
+
+- And render function to recall data to assigned areas on page refresh.
+![media_Q.1](https://github.com/ajhuff7/portfolio-one/blob/master/assets/images/media_Q.1.JPG)
+
+
+- Text areas are linked to dynamic clock to display entries as past, present, and future. 
+
+  ![response.1](https://github.com/ajhuff7/portfolio-one/blob/master/assets/images/response.1.JPG)
+
+
+![media_Q.3](https://github.com/ajhuff7/portfolio-one/blob/master/assets/images/media_Q.3.JPG)
+
+## Deployed Link
+
+* [See Live Site](https://ajhuff7.github.io/work-day-scheduler/)
+
+---
+
+## Author
+
+**AJ Huff** 
+
+- [Portfolio Site](#)
+- [Github](https://github.com/ajhuff7)
+- [LinkedIn](https://www.linkedin.com/in/aj-huff-7696b14b/)
+
+## Credits
+
+**Huge thank you to team members, coaches and staff!**
+
+## License
+
+![License](https://img.shields.io/badge/License-MIT-brightgreen) 
--- script.js
+++ script.js
@@ -14,27 +14,36 @@ $(document).ready(function () {
     // This will keep the clock running...
     setInterval(updateTime, 1000);
     
+
     
     // This saves input to local storage
-    var textarea = $("#textarea")
-    var input = textarea.value;
 
-    $("#saveBtn").on("click", function () {
+    $(".saveBtn").on("click", function () {
+        var textareaId = $(this).data("target");
+        var textarea = $("#" + textareaId);
 
-        localStorage.setItem("textarea", input);
-        localStorage.getItem("textarea", input);
-        input.append(textarea)
+        localStorage.setItem(textareaId, textarea.val());
+        
     })
 
     // This retrieves text input to calendar
 
+    // function render(){
+    //     var textareaId = $(this).data("target");
+    //     var textarea = $("#" + textareaId);
+        
+    //     localStorage.getItem(textareaId);
+    //     textarea.append(textarea.val());
+    // }
+    
+    // render();
 
 
     // These variables hold my times values
     var currentHour = moment().format('hh');
-    var lateNight = moment().format('11:59:59 pm');
-    var newDay = moment().format('hh:mm:ss a');
-    var calendarHour = [09, 10, 11, 12, 01, 02, 03, 04, 05]
+    // var lateNight = moment().format('11:59:59 pm');
+    // var newDay = moment().format('hh:mm:ss a');
+    var calendarHour = ["09", "10", "11", "12", "01", "02", "03", "04", "05"]
     // console.log(currentHour)
     // console.log(lateNight)
     // console.log(newDay)
@@ -43,70 +52,130 @@ $(document).ready(function () {
     // This function changes the highlighted entries based on TOD.
 
     function currentTime() {
+    console.log(currentHour, calendarHour.indexOf(currentHour),calendarHour.indexOf("09"))
 
-        for (var i = 0; i < calendarHour.length; i++) {
-        if (currentHour === calendarHour) {
+        if (currentHour === "09") {
             $("#09").removeClass("future").addClass("present");
+        } 
+        
+        else if (calendarHour.indexOf("09") < calendarHour.indexOf(currentHour)) {
+            $("#09").removeClass("present").addClass("past");
         }
 
-        if (currentHour === calendarHour) {
+        else {
+            $("#09").addClass("future");
+
+        }
+
+        if (currentHour === "10") {
             $("#10").removeClass("future").addClass("present");
+        } 
+        
+        else if (calendarHour.indexOf("10") < calendarHour.indexOf(currentHour)) {
+            $("#10").removeClass("present").addClass("past");
         }
 
-        if (currentHour === calendarHour) {
+        else  {
+            $("#10").addClass("future");
+
+        }
+        
+        if (currentHour === "11") {
             $("#11").removeClass("future").addClass("present");
+        } 
+        
+        else if (calendarHour.indexOf("11") < calendarHour.indexOf(currentHour)) {
+            $("#11").removeClass("present").addClass("past");
         }
 
-        if (currentHour === calendarHour) {
+        else {
+            $("#11").addClass("future");
+
+        }
+
+        if (currentHour === "12") {
             $("#12").removeClass("future").addClass("present");
+        } 
+        
+        else if (calendarHour.indexOf("12") < calendarHour.indexOf(currentHour)) {
+            $("#12").removeClass("present").addClass("past");
         }
 
-        if (currentHour === calendarHour) {
+        else {
+            $("#12").addClass("future");
+
+        }
+
+        if (currentHour === "01") {
             $("#01").removeClass("future").addClass("present");
+        } 
+        
+        else if (calendarHour.indexOf("01") < calendarHour.indexOf(currentHour)) {
+            $("#01").removeClass("present").addClass("past");
         }
 
-        if (currentHour === calendarHour) {
+        else {
+            $("#01").addClass("future");
+
+        }
+
+        if (currentHour === "02") {
             $("#02").removeClass("future").addClass("present");
+        } 
+        
+        else if (calendarHour.indexOf("02") < calendarHour.indexOf(currentHour)) {
+            $("#02").removeClass("present").addClass("past");
         }
 
-        if (currentHour === calendarHour) {
+        else {
+            $("#02").addClass("future");
+
+        }
+
+        if (currentHour === "03") {
             $("#03").removeClass("future").addClass("present");
+        } 
+        
+        else if (calendarHour.indexOf("03") < calendarHour.indexOf(currentHour)) {
+            $("#03").removeClass("present").addClass("past");
         }
 
-        if (currentHour === calendarHour) {
+        else {
+            $("#03").addClass("future");
+
+        }
+
+        if (currentHour === "04") {
             $("#04").removeClass("future").addClass("present");
+        } 
+        
+        else if (calendarHour.indexOf("04") < calendarHour.indexOf(currentHour)) {
+            $("#04").removeClass("present").addClass("past");
         }
 
-        if (currentHour === calendarHour) {
+        else {
+            $("#04").addClass("future");
+
+        }
+
+        if (currentHour === "05") {
             $("#05").removeClass("future").addClass("present");
+        } 
+        
+        else if (calendarHour.indexOf("05") < calendarHour.indexOf(currentHour)) {
+            $("#05").removeClass("present").addClass("past");
         }
 
-        if (lateNight === newDay) {
-            $(".future").addClass("future");
+        else {
+            $("#05").addClass("future");
+
         }
-    }
+
+    
     };
     
     currentTime ();
 
-
-
-
-
-    //     }
-    // })
-    
-    // var hour = $(this).data("time")
-    // time = [9, 10, 11, 12, 1, 2, 3, 4, 5]
-
-    // function getExpiredTime(time) {
-    //     for (i = 0; i < time.length; i++) {
-    //         if (time[i] === hour) {
-    //             $(this).attr("style", "background: red")
-    //         }
-    //     }
-    // }
-    
   
     // getExpiredTime();
     // getExpireTime();
